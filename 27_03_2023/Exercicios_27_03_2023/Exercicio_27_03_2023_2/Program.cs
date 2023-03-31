6d1d197f41df793358889eb7b93c52ba6b20e417
Jonatã Serdan Marino <jsmarino1993@outlook.com>
2023-03-31 10:01:54 -0300

    31_03_23

---
+++ 27_03_2023/Exercicios_27_03_2023/Exercicio_27_03_2023_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercicio_27_03_2023_2;
+
+
+Imensagem email = new Email();
+Console.WriteLine("Conteudo do email: ");
+string mail = Console.ReadLine();
+Console.WriteLine("Enviando e-mail...");
+string msg = "email que era pra ir com anexo mas foi apenas com a mensagem kkkkk";
+email.EnviarMensagem(msg);
+Console.WriteLine($"email enviado com sucesso:[ {mail} ]\nObservação: {msg}");
+
+
+//Console.WriteLine("Enviando e-mail...");
